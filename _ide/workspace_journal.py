a9674f4e75e9993f86cb7fe4bc64a041f493f56d
thắng <app443325@gmail.com>
2025-11-09 13:02:59 +0700

    2nd test

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -1,8 +1,6 @@
-# 2025-11-08T00:59:37.884368800
+# 2025-11-09T12:43:59.637542600
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="vitiswork")
-
-comp = client.create_app_component(name="app_component",platform = "$COMPONENT_LOCATION/../ARTY/export/ARTY/ARTY.xpfm",domain = "standalone_ps7_cortexa9_0")
 
